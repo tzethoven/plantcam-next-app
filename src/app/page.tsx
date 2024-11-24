bdfc2620d50a23a9a061991b5b95ac67383682ec
Tobias Zethoven <tzethoven@gmail.com>
2024-11-24 23:04:50 +0100

    dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import { useState } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   const [loading, setLoading] = useState(false);
 
