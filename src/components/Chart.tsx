24898b4ebd2945486844912c0abdaf1f57c650f6
Tobias Zethoven <tzethoven@gmail.com>
2024-11-27 18:01:54 +0100

    remove footer

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -82,7 +82,7 @@ export const Chart = ({ chartData, chartConfig }: Props) => {
         </ChartContainer>
       </CardContent>
       <CardFooter>
-        <div className="flex w-full items-start gap-2 text-sm">
+        {/* <div className="flex w-full items-start gap-2 text-sm">
           <div className="grid gap-2">
             <div className="flex items-center gap-2 font-medium leading-none">
               Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
@@ -91,7 +91,7 @@ export const Chart = ({ chartData, chartConfig }: Props) => {
               January - June 2024
             </div>
           </div>
-        </div>
+        </div> */}
       </CardFooter>
     </Card>
   );
